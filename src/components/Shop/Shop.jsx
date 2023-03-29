f2b585d2ea7efe46ed41e5c6a9ed0a8aa405226b
manikonline <thmanik1241@gmail.com>
2023-03-29 21:55:46 +0600

    local storage task

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -19,15 +19,21 @@ const Shop = () => {
 
     useEffect(()=>{
         const storedCart=getShoppingCart();
+        const savedCart=[];
         // Step 1: get id
         for(const id in storedCart){
         // step 2: get the product by using id
         const addedProduct=products.find(product=>product.id===id);
         // step 3: get quantity of the product
-        const quantity=storedCart[id];
-        addedProduct.quantity=quantity
+        if(addedProduct){
+            const quantity=storedCart[id];
+        addedProduct.quantity=quantity;
+        // step 4: add the added product to the saved cart
+        savedCart.push(addedProduct);
         }
-    },[])
+        }
+        setCart(savedCart)
+    },[products])
     const addTOProductCart=(product)=>{
         const newCart=[...cart ,product]
         setCart(newCart);
