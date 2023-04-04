4552f8311a7422a058cfd7b34d47228514fc1bf9
manikonline <thmanik1241@gmail.com>
2023-04-05 03:07:50 +0600

    add Orders.jsx and cartProductsLoader.js file

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import './Header.css'
 import logo from '../../images/Logo.svg'
+import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <nav className='Header'>
            <img src={logo} alt="" />
            <div>
-            <a href="/Order">Order</a>
-            <a href="/Oder Review">Oder Review</a>
-            <a href="/Manage Inventory">Manage Inventory</a>
-            <a href="/login">login</a>
+            <Link to="/">Shop</Link>
+            <Link to="/orders">Orders</Link>
+            <Link to="/inventory">Inventory</Link>
+            <Link to="/login">login</Link>
            </div>
         </nav>
     );
